b5b2c265cf563395993353748f6be2230682ed66
Victoria-Gudoit <vgudojt@mail.ru>
2022-05-26 23:47:19 +0300

    add typescript

--- src/components/Todos/Todos.tsx
+++ src/components/Todos/Todos.tsx
@@ -2,7 +2,7 @@ import React, { ChangeEventHandler } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { TodosSelectors, Todo, TodosActions, FilterAction } from "../../store";
 import { FILTER } from "../../store/filterSlice";
-import {CheckboxGroup} from "../Checkbox"
+import { CheckboxGroup } from "../Checkbox";
 
 interface TodosProps {
   name: string;
@@ -22,40 +22,38 @@ export const Todos: React.FC<TodosProps> = ({ name }) => {
     dispatch(TodosActions.deleteTodo({ id }));
   };
 
-  const changeFilter: ChangeEventHandler<HTMLInputElement> = (event: any) => { //тут я что-то не сообрзила как по-другому
+  const changeFilter: ChangeEventHandler<HTMLInputElement> = (event: any) => {//тут я что-то не сообрзила как по-другому
     dispatch(FilterAction.toggleFilter({ value: event.target.value }));
   };
 
   return (
     <div>
-      
       <div>
         <CheckboxGroup
           options={(Object.keys(FILTER) as (keyof typeof FILTER)[]).map(
             (key) => {
-              return key
-            },
+              return key;
+            }
           )}
           value={filter}
           onChange={changeFilter}
         />
       </div>
-       <h1>{name}, что тебе надо сделать:</h1>
+      <h1>{name}, что тебе надо сделать:</h1>
       <ul>
-        {
-          todos.map(({ id, label, isDone }) => (
-            <li key={id}>
-              <label>
-                <input
-                  type="checkbox"
-                  checked={isDone}
-                  onChange={() => toggleTask(id)}
-                />
-                {label}
-                <button onClick={deleteTask.bind(null, id)}>Удалить</button>
-              </label>
-            </li>
-          ))}
+        {todos.map(({ id, label, isDone }) => (
+          <li key={id}>
+            <label>
+              <input
+                type="checkbox"
+                checked={isDone}
+                onChange={() => toggleTask(id)}
+              />
+              {label}
+              <button onClick={deleteTask.bind(null, id)}>Удалить</button>
+            </label>
+          </li>
+        ))}
       </ul>
     </div>
   );
